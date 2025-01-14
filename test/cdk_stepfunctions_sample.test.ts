327e4b10b0810b69f69ec3830fe96eb7dc09719d
EmiRoberti77 <emi@datamotion.ai>
2025-01-14 19:00:53 +0000

    Initial commit

---
+++ test/cdk_stepfunctions_sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStepfunctionsSample from '../lib/cdk_stepfunctions_sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_stepfunctions_sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStepfunctionsSample.CdkStepfunctionsSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
